8a56dec5fe650c8303880957b74c7014b502103d
George Gkekis <georgegkekis@gmail.com>
2016-03-01 13:09:10 +0000

    changed mac address to be a variable

--- receiver.py
+++ receiver.py
@@ -2,22 +2,24 @@ import socket
 import sys
 import re
 
+mac = '001723f14717'
+
 def check_version(mydata):
     if len(mydata) != 2:
         return -1
-    if mydata[0] == '001723f14717' and mydata[1] == '*VERSION:2.4.0':
+    if mydata[0] == mac and mydata[1] == '*VERSION:2.4.0':
         return 1
     else:
         return -1
 
 def check_loopdetection(mydata):
-    if mydata[0] == '001723f14717' and mydata[1] == '*LOOPDETECTION':
+    if mydata[0] == mac and mydata[1] == '*LOOPDETECTION':
         return 1
     else:
         return -1
 
 def check_status(mydata):
-    if mydata[0] == '001723f14717' and mydata[1] == '*STATUS':
+    if mydata[0] == mac and mydata[1] == '*STATUS':
         return 1
     else:
         return -1
@@ -25,14 +27,14 @@ def check_status(mydata):
 def check_heartbeat(mydata):
     if len(mydata) != 2:
         return -1
-    if mydata[0] == '001723f14717' and mydata[1] == '*WB45HB':
+    if mydata[0] == mac and mydata[1] == '*WB45HB':
         return 1
     else:
         return -1
 
 def check_survey(mydata):
     surveylist =[]
-    while mydata[0] == '001723f14717' and mydata[1] == '*SITESURVEY': 
+    while mydata[0] == mac and mydata[1] == '*SITESURVEY': 
         print >>sys.stderr, 'received a valid survey:\nMAC address:%s\nSSID:%s' %(mydata[0],mydata[2])   
         surveylist.append(mydata)
         data, address = sock.recvfrom(1024)
@@ -73,6 +75,6 @@ while True:
 
     if valid > 0: print 'data  valid'
     else: print 'data not valid'
-    print >>sys.stderr, '\nsending acknowledgement to', address
+    print >>sys.stderr, 'sending acknowledgement to', address
     sock.sendto('ack', address)
 
